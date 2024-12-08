39ea86537567860027abb948569b65c22cc4d085
apainintheneck <apainintheneck@gmail.com>
2024-12-08 11:22:38 -0800

    Upgrade search command spec

--- spec/gemview/commands_spec.rb
+++ spec/gemview/commands_spec.rb
@@ -95,16 +95,61 @@ RSpec.describe Gemview::Commands do
   end
 
   describe "search" do
+    let(:birdbrain_selector) do
+      "birdbrain [0.9.6]\n  -- This Ruby library allows students to use Ruby to read sensors and set mot…\n"
+    end
+    let(:birdbrain_gem) do
+      Gemview::Gem.new({
+        "documentation_uri": nil,
+        "metadata": {
+          "homepage_uri": "https://github.com/fmorton/BirdBrain-Ruby-Library",
+          "allowed_push_host": "https://rubygems.org",
+          "changelog_uri": "https://github.com/fmorton/BirdBrain-Ruby-Library/CHANGELOG.md",
+          "source_code_uri": "https://github.com/fmorton/BirdBrain-Ruby-Library"
+        },
+        "homepage_uri": "https://github.com/fmorton/BirdBrain-Ruby-Library",
+        "funding_uri": nil,
+        "bug_tracker_uri": nil,
+        "project_uri": "https://rubygems.org/gems/birdbrain",
+        "version": "0.9.6",
+        "sha": "c5bdeae5072c0c4e80acbd6c06d4242ef299a22d8254d458ecc6cff89cf05a21",
+        "platform": "ruby",
+        "changelog_uri": "https://github.com/fmorton/BirdBrain-Ruby-Library/CHANGELOG.md",
+        "source_code_uri": "https://github.com/fmorton/BirdBrain-Ruby-Library",
+        "licenses": [
+          "MIT"
+        ],
+        "gem_uri": "https://rubygems.org/gems/birdbrain-0.9.6.gem",
+        "downloads": 13313,
+        "mailing_list_uri": nil,
+        "name": "birdbrain",
+        "wiki_uri": nil,
+        "version_downloads": 2520,
+        "info": "This Ruby library allows students to use Ruby to read sensors and set motors and LEDs with the Birdbrain Technologies Hummingbird Bit and Finch 2. To use Ruby with the Hummingbird Bit or Finch 2, you must connect via bluetooth with the BlueBird Connector.",
+        "authors": "fmorton"
+      })
+    end
+
     it "completes search" do
       expect(Gemview::Terminal)
         .to receive(:choose)
         .with(
           message: "What gem would you like to look at?",
-          choices: an_instance_of(Hash)
+          choices: hash_including(birdbrain_selector => birdbrain_gem)
+        )
+        .and_yield(birdbrain_gem)
+
+      expect(Gemview::Terminal)
+        .to receive(:choose)
+        .with(
+          message: match_snapshot("birdbrain-gem-header"),
+          choices: %w[Readme Changelog Dependencies Versions]
         )
 
       VCR.use_cassette("search-for-bluetooth") do
-        described_class.start(arguments: %w[search bluetooth])
+        VCR.use_cassette("find-birdbrain-gem") do
+          described_class.start(arguments: %w[search bluetooth])
+        end
       end
     end
   end
